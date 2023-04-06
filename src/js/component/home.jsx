12199b1cf1a2382893216478bde1e23983ee1d30
yanzacd92 <yanzacd@gmail.com>
2023-04-06 02:38:19 +0000

    add todo list

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, {useState, useRef} from "react";
+import { Title } from "./title.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const ref = useRef(null);
+	const [task, setTask] = useState("");
+	const [todoList, setTodoList] = useState([]);
+
+	function addItem(e) {
+		if(e.code =="Enter") {
+			setTodoList(todoList => [...todoList, task])
+			setTask("")
+		}
+	}
+	
+	function removeItem(index) {
+		setTodoList([
+			...todoList.slice(0, index),
+			...todoList.slice(index + 1, todoList.length)
+		  ]);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Title />
+			<div className="card">
+				<div className="card-header">
+				<input ref={ref} type="text" className="form-control" id="inputControl" 
+					placeholder="What needs to be done?"
+					value={task} onChange={(e) => setTask(e.target.value)}
+					onKeyDown={addItem}/>
+				</div>
+				<ul className="list-group list-group-flush">
+					{
+						todoList.map((todo, index) => (
+							<li key={index} className="list-group-item d-flex justify-content-between align-item-center">
+								{todo}
+								<button type="button" onClick={() => removeItem(index)} className="badge rounded-pill">x</button>
+							</li>
+						))
+					}
+				</ul>
+				<div className="card-footer">
+					{todoList.length} item left
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ src/js/component/title.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+//create your first component
+export const Title = () => {
+	return (
+		<h1 className="title">TODO List</h1>
+	);
+};
