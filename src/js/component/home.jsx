24c827f8e3c72496ab10fbe09468f26d0bde72e4
yanzacd92 <121258474+yanzacd92@users.noreply.github.com>
2023-04-22 22:00:49 +0000

    Adding logic to API TODO LIST

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import { Title } from "./title.jsx";
 
 
@@ -7,10 +7,53 @@ const Home = () => {
 	const ref = useRef(null);
 	const [task, setTask] = useState("");
 	const [todoList, setTodoList] = useState([]);
+	const apiURL = "https://assets.breatheco.de/apis/fake/todos/user/";
+	const username = "yanzacd"
+
+	useEffect(() => {
+		getItems()
+	}, [])
+
+	function getItems() {
+		const requestOptions = {
+			method: 'GET'
+		  };
+		  
+		  fetch(apiURL + username, requestOptions)
+			.then( response => {
+				if(response.ok) {
+					// response passed
+					return response.json()
+				} else if(response.status == "404"){
+					return [].json()
+				} else {
+					// response failed
+					console.log(response.status + ": " + response.statusText)
+				}
+			})
+			.then(data => {
+				setTodoList(data)
+			})
+			.catch(error => console.log('error', error));
+	}
 
 	function addItem(e) {
 		if(e.code =="Enter") {
-			setTodoList(todoList => [...todoList, task])
+			/*const myHeaders = new Headers();
+			myHeaders.append("Content-Type", "application/json");
+			const raw = JSON.stringify([]);
+			const requestOptions = {
+				method: 'POST',
+				headers: myHeaders,
+				body: raw
+			};
+
+			fetch(apiURL + username, requestOptions)
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log('error', error));*/
+
+			setTodoList(todoList => [...todoList, {label: task, done: false}])
 			setTask("")
 		}
 	}
@@ -20,6 +63,12 @@ const Home = () => {
 			...todoList.slice(0, index),
 			...todoList.slice(index + 1, todoList.length)
 		  ]);
+	}
+
+	function checkTodo(index) {
+		let newTodoList = [...todoList]
+		newTodoList[index].done = !newTodoList[index].done
+		setTodoList(newTodoList)
 	}
 
 	return (
@@ -36,7 +85,10 @@ const Home = () => {
 					{
 						todoList.map((todo, index) => (
 							<li key={index} className="list-group-item d-flex justify-content-between align-item-center">
-								{todo}
+								<div>
+									<input className="form-check-input me-3" type="checkbox" value="" checked={todo.done} id="flexCheckDefault" onChange={() => checkTodo(index)} />
+									{todo.label}
+								</div>
 								<button type="button" onClick={() => removeItem(index)} className="badge rounded-pill">x</button>
 							</li>
 						))
